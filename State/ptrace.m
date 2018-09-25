3345281d79d0450e1246cb6b3ae4b168195f4de0
Herianto Lim <mail@heriantolim.com>
2018-09-26 09:39:46 +1000

    Add flexibility in ptrace

--- State/ptrace.m
+++ State/ptrace.m
@@ -1,10 +1,11 @@
 function pn=ptrace(p,d,n)
 %% Partial Trace
 %  pn=ptrace(p,d,n) computes the partial trace of the density matrix, p, over
-%  the subsystem of index n. The input to p can also be a state (a complex
+%  the subsystem(s) of index n. The input to p can also be a state (a complex
 %  vector), in which case p will be parsed as p*p'. The integer vector d is the
-%  list of the subspace dimensions. This function is a complementary operation
-%  to subdsmat.
+%  list of the subspace dimensions. The index n can be inputted as an integer
+%  scalar or vector. The elements in n must not have duplicates. This function
+%  is a complementary operation to subdsmat.
 %
 % See also: subdsmat.
 %
@@ -36,29 +37,42 @@ assert(D==prod(d),...
 		'equal to the dimension of the density matrix.']);
 N=numel(d);
 
-assert(isintegerscalar(n) && n>0 && n<=N,...
+assert(isintegervector(n) && all(n>0 & n<=N) && numel(unique(n))==numel(n),...
 	'FluxQon:State:ptrace:InvalidInput',...
-	['Input to the subspace index must be an integer scalar between 1 ',...
-		'and the total number of the subspaces.']);
+	['Input to the subspace index must be an integer scalar or vector, ',...
+		'with no duplicates, between 1 and the total number of the subspaces.']);
 
 if N==1
 	pn=trace(p);
 	return
 end
-d=[prod(d(1:(n-1))),d(n),prod(d((n+1):N))];
 
-D=d(2)*d(3);
-pn=zeros(d(1)*d(3));
-for i=1:d(1)
-	for j=1:d(1)
-		for m=1:d(2)
-			for k=1:d(3)
-				for l=1:d(3)
-					pn((i-1)*d(3)+k,(j-1)*d(3)+l)=pn((i-1)*d(3)+k,(j-1)*d(3)+l) ...
-						+p((i-1)*D+(m-1)*d(3)+k,(j-1)*D+(m-1)*d(3)+l);
+while true
+	q=[prod(d(1:(n(1)-1))),d(n(1)),prod(d((n(1)+1):N))];
+	r=q(2)*q(3);
+	pn=zeros(q(1)*q(3));
+	for i=1:q(1)
+		for j=1:q(1)
+			for m=1:q(2)
+				for k=1:q(3)
+					for l=1:q(3)
+						pn((i-1)*q(3)+k,(j-1)*q(3)+l)=pn((i-1)*q(3)+k,(j-1)*q(3)+l) ...
+							+p((i-1)*r+(m-1)*q(3)+k,(j-1)*r+(m-1)*q(3)+l);
+					end
 				end
 			end
 		end
+	end
+	
+	if numel(n)>1
+		p=pn;
+		d(n(1))=[];
+		N=N-1;
+		ix=n>n(1);
+		n(ix)=n(ix)-1;
+		n(1)=[];
+	else
+		break
 	end
 end
 
